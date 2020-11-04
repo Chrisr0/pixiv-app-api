b2efd63a11975ab4cf426964f29ea6d2b51f66df
Chrisr0 <36136394+Chrisr0@users.noreply.github.com>
2020-11-04 13:15:27 +0100

    test

--- dist/index.js
+++ dist/index.js
@@ -22,13 +22,13 @@ const axios_1 = __importDefault(require("axios"));
 const decamelize_keys_1 = __importDefault(require("decamelize-keys"));
 const camelcase_keys_1 = __importDefault(require("camelcase-keys"));
 const baseURL = 'https://app-api.pixiv.net/';
-const appVersion = '7.2.5';
+const appVersion = '7.9.7';
 const userAgent = `PixiviOSApp/${appVersion} (iOS 14.1; PixivBot)`;
 const instance = axios_1.default.create({
     baseURL,
     headers: {
         'App-OS': 'ios',
-        'App-OS-Version': '7.9.5',
+        'App-OS-Version': '7.9.7',
         'App-Version': appVersion,
         'User-Agent': userAgent,
     },
